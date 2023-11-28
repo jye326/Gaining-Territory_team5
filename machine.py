db6bd46dcb24bf82baafd34859472753f0fcaaf1
jeremyjclee <111248604+jeremyjclee@users.noreply.github.com>
2023-11-28 21:28:34 +0900

    Update machine.py
    # 변경사항
    1 trick 관련 함수 추가 (다른 부분들을 삭제하거나 건든 부분 X)
    
    # 디버깅 및 논의가 필요한 부분들
    0 trick 관련 함수 아직 제대로 작동 X
    
    1 def organize_points(self, point_list):
    point_list.sort(key=lambda x: (x[0], x[1])) 
    -> point_list가 튜플이라서 sort 함수를 사용할 수 없다는 메시지
    
    2 def find_best_selection(self):
    -> rule 기반이 모두 합쳐진 상태를 고려한 find_best_selection 새로 만들어야 함
    
    3 얕은 복사와 깊은 복사
    얕은 복사로 self.drawn_lines를 쓰면 pointer를 이용하는 것이라 잘 못하면 self.drawn_lines의 값이 조작될 수도 있음.
    (c = self.drawn_lines 한 후, c의 값을 변경하면 self.drawn_lines에도 영향)
    
    관련 링크
    https://crackerjacks.tistory.com/14

--- machine.py
+++ machine.py
@@ -2,6 +2,8 @@ import random
 from itertools import combinations
 from shapely.geometry import LineString, Point
 
+import copy  # for deepcopy
+
 
 class MACHINE():
     """
@@ -31,6 +33,8 @@ class MACHINE():
         self.sim_triangles = []
         #print(self.avail_lines_num)
         #222#
+
+        self.drawn_lines_copy = copy.deepcopy(drawn_lines)
         
 
         
@@ -153,6 +157,156 @@ class MACHINE():
     def find_available_lines(self):
         available = [[point1, point2] for (point1, point2) in list(combinations(self.whole_points, 2)) if self.check_availability([point1, point2])]
         return available
+    '''
+    여기서부터 trick 관련 함수
+    '''
+    just_3 = list(combinations(self.whole_points, 3))
+    triangle_point = []                               # point of completing a triangle - (1,1), (1,4), (4,4)
+    trick_point = []                                  # point of completing a trick - (1,1), (1,4), (4,4), (2,2)
+    avail_trick_point = []
+    most_completed_trick = []                       # point of completing the most completed trick
+    trick_line = []                                 # line needed to complete the trick 
+    
+    # function of calculating triangle area
+    def area(p1, p2, p3):
+        area_val = 0.5 * abs(p1[0]*p2[1] + p2[0]*p3[1] + p3[0]*p1[1] - p1[0]*p3[1] - p3[0]*p2[1] - p2[0]*p1[1])
+        return area_val
+    
+    # find triangle_point
+    for p in just_3:
+        p_set = list(p)
+        p1, p2, p3 = p_set[0], p_set[1], p_set[2]
+        area_val = area(p1, p2, p3)
+        line = []
+        line1 = LineString([p1,p2]) ; line2 = LineString([p1,p3]) ; line3 = LineString([p2,p3])
+        line.append(line1) ; line.append(line2); line.append(line3)
+        if area_val > 0:
+          i=0
+          for point in self.whole_points: # 선분 안에 점이 들어가는 삼각형 필터링
+            if point == p1 or point == p2 or point == p3:
+              continue
+            else:
+              if bool(line1.intersection(Point(point))) or bool(line2.intersection(Point(point))) or bool(line3.intersection(Point(point))):
+                i += 1
+          if i == 0:  
+            triangle_point.append(p_set)       
+        
+    # find trick_point
+    for i, p in enumerate(triangle_point): # 삼각형 안에 점이 1개 들어가면 trick_point에 저장
+        inside_num = 0
+        for q in self.whole_points: #
+            if q not in p:
+                if area(p[0],p[1],p[2]) == area(p[0],p[1],q) + area(p[0],q,p[2]) + area(q,p[1],p[2]):
+                    inside_num += 1
+                    inside_point = q
+        if inside_num == 1:
+            p_copy = p.copy()  # p를 복사해서 새로운 리스트 생성
+            p_copy.append(inside_point)  
+            trick_point.append(p_copy)  
+            
+    # find trick_line
+    i=0
+    for p in trick_point:
+        trick_line.append(list(combinations(p, 2)))
+        
+    # validate trick_point
+    for j in range(0,len(trick_point)): # 유효한 trick만 avail_trick_point에 저장: trick이 깨졌거나 trick에 걸리는 상황은 제외
+        outer_point = list(trick_point[j][0:3])
+        total_line = list(combinations(trick_point, 2))
+        outside_line = list(combinations(outer_point,2))
+        inside_line = [i for i in total_line if i not in outside_line]
+    
+        out_drawen_check = [line in self.drawn_lines_copy for line in outside_line]
+        in_drawen_check = [line in self.drawn_lines_copy for line in inside_line]
+        out_count = sum(out_drawen_check)
+        in_count = sum(in_drawen_check)
+    
+        avail_candidate = list(trick_point[j])
+    
+        if out_count == 3:
+            if in_count == 0 or 2:
+                avail_trick_point.append(avail_candidate)
+        else:
+            if in_count == 0:
+                avail_trick_point.append(avail_candidate)
+    print(f"avail_trick_point is {avail_trick_point}")
+
+    # find most_completed_trick
+    highest_count = -1
+    for j in range(0,len(avail_trick_point)):
+        total_line = list(combinations(avail_trick_point[j],2))
+        out_line = list(combinations(avail_trick_point[j][0:3],2))
+        in_line = [i for i in total_line if i not in out_line]
+        trick_candidate = []
+    
+        alredy_line = [tuple(sorted(line)) for line in self.drawn_lines_copy]
+        total_line = [tuple(sorted(line)) for line in total_line]
+    
+        in_check = [line in alredy_line for line in in_line] ; in_count = sum(in_check)
+        out_check = [line in alredy_line for line in out_line] ; out_count = sum(out_check)
+        count = in_count + out_count
+        
+        first_check=0
+    
+        if out_count == 3:
+          if in_count == 1 or in_count == 3: # 이미 선이 모드 그어졌거나 trick에 걸리는 상황 그냥 pass
+              pass
+          elif in_count == 2:
+            trick_candidate.append(avail_trick_point[j])
+          elif in_count == 0:
+            trick_candidate.append(avail_trick_point[j])                  
+        else:
+          if in_count == 0:
+            trick_candidate.append(avail_trick_point[j])
+
+        for i in range(sum(len(sublist) for sublist in trick_candidate)):
+          total_line = list(combinations(avail_trick_point[i],2))
+          count = sum([(line in alredy_line) for line in total_line])
+          if count > highest_count:
+            most_completed_trick = []
+            most_completed_trick.append(trick_candidate[0][i])
+            highest_count = count
+          elif count == highest_count:
+            most_completed_trick.append(trick_candidate[0][i])
+
+    sub_factory = []
+    for i in range(0, len(most_completed_trick), 4):
+      sub_factory.append(most_completed_trick[i:i+4])
+    most_completed_trick = sub_factory
+    return most_completed_trick
+        
+    
+    def complete_trick(self, most_completed_trick):
+        best_trick_line = []
+        for i in range(len(most_completed_trick)):
+            outer_point = list(most_completed_trick[i][0:3])
+            total_line = list(combinations(most_completed_trick[i], 2))
+            outside_line = list(combinations(outer_point,2))
+            inside_line = [i for i in total_line if i not in outside_line]
+        
+            out_drawen_check = [line in self.drawn_lines_copy for line in outside_line]
+            in_drawen_check = [line in self.drawn_lines_copy for line in inside_line]
+            out_count = sum(out_drawen_check)
+            in_count = sum(in_drawen_check)
+        
+            if out_count == len(outside_line):  # 바깥에 있는 점이 모두 연결되어 있는 경우
+              if in_count == 0 or in_count == 2:  # 안쪽에 선이 0개 또는 2개 완성되어 있을 때
+                  candidate = [c for c in inside_line if c not in self.drawn_lines_copy]
+                  best_trick_line.append(candidate)
+            else:  # 바깥에 있는 점이 모두 연결되어 있지 않은 경우
+                candidate = [c for c in outside_line if c not in self.drawn_lines_copy]
+                best_trick_line.append(candidate)
+        
+        # 하위 리스트를 허물어 저장할 새로운 리스트 생성
+        flattened_best_trick_line = []
+        
+        # 각 하위 리스트의 원소를 새로운 리스트에 추가
+        for sublist in best_trick_line:
+            for item in sublist:
+                flattened_best_trick_line.append(item)
+    
+        return flattened_best_trick_line
+
     
 # Minmax Tree Node
 class Node:
@@ -207,11 +361,9 @@ class Node:
                 if beta <= alpha:
                     break
             return minEval
-            
 
 
-
-
+        
 # node - 위에 구현한 Node를 사용하면 될 듯 
 # depth - 임의로 설정
 # isMaximizingPlayer -> turn확인 후 내 turn일때 true넣자
